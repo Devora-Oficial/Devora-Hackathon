58bc2575d9bb7f86e35de86b071b5ef7b2d0d6a7
ElielZinKwaiProGamer123 <elielmurbach01@gmail.com>
2025-12-03 02:37:15 -0300

    Teste 2 Login
    sla

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   // -- Tela Login --
  
   return (
-    <div className="min-h-screen flex bg-slate-700 sh">
+    <div className="min-h-screen flex bg-slate-700 shadow-xl rounded-b-lg">
       <div className="w-1/2 flex items-center justify-center">
         <div className="w-full max-w-sm bg-slate-800 p-14 rounded-2xl shadow-xl border border-slate-800">
           <h2 className="text-2xl font-bold text-center mb-6 text-white">Lolgin</h2>
@@ -18,16 +18,16 @@ function App() {
               <label className="block text-sm text-white font-medium mb-1">PASword</label>
               <input type="password" className="w-full px-3 py-2 border rounded-lg text-white" placeholder="••••••••" />
             </div>
-            <button className="w-full py-2 bg-purple-700 text-white font-semibold rounded-lg">Entrada</button>
+            <button className="w-full py-2 bg-purple-700 text-white font-semibold rounded-lg">EntraAikk</button>
             <p className="text-center text-sm text-white mt-3 ">Sem Contafdskk <a className="text-purple-600 underline cursor-pointer">Criar ai</a></p>
           </div>
         </div>
       </div>
  
       <div className="w-1/2 relative  overflow-hidden flex items-center justify-center">
-        <div className="absolute inset-0 bg-slate-900 bg-center">
+        <div className="absolute inset-0  bg-gradient-zen bg-center">
         </div>
-        
+
         <div className="absolute bg-blue-700"></div>
  
         <div className="relative text-white px-16">
