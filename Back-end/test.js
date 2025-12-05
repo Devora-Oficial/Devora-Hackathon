ba1ba794aaed38f40c3ac5dddda7acd56bac5291
Mateus Storti Hellmann <mashuu.sutoruti@gmail.com>
2025-12-05 01:23:58 -0400

    a

--- Back-end/test.js
+++
@@ -1,8 +0,0 @@
-const bcrypt = require("bcrypt");
-
-const senha = "123456";
-const hash = "$2b$10$Cw9Wq5JZf4xP9gO8aN4T6OXVyhi0SKxF7hF5ZWSr1L6MjYSY4OwFi";
-
-bcrypt.compare(senha, hash)
-  .then(result => console.log("Resultado compare:", result))
-  .catch(err => console.error(err));
--- Front-end/src/utils/cn.js
+++
@@ -1,6 +0,0 @@
-// utils/cn.js
-
-// Função utilitária para combinar classes CSS
-export function cn(...classes) {
-  return classes.filter(Boolean).join(' ');
-}
